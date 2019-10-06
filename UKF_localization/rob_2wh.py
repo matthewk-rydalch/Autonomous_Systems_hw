4e60e75531980344e157767bc71bc4083f447209
Matt Rydalch <matthewk.rydalch@gmail.com>
2019-10-05 23:33:07 -0600

    organized

--- UKF_localization/rob_2wh.py
+++ UKF_localization/rob_2wh.py
@@ -22,6 +22,7 @@ pd.set_option('display.max_columns',10)
 
 class Rob2Wh:
     def __init__(self):
+        # parameters
         self.a1 = 0.1
         self.a2 = 0.01
         self.a3 = 0.01
@@ -53,6 +54,19 @@ class Rob2Wh:
         self.y0 = -3
         self.th0 = math.pi/2 #rad
 
+        #generate sigma points
+        self.n = 7 # #u+#x+#z
+
+        # k = 1 # no specific values for k and alpha.  I will use the recommended values on the slides
+        # alpha = .5 #?
+        # Beta = 2
+        self.k = 4 # no specific values for k and alpha.  I will use the recommended values on the slides
+        self.alpha = .4 #?
+        self.Beta = 2
+
+        self.lam = self.alpha**2*(self.n+self.k)-self.n
+        self.gm = np.sqrt(self.n+self.lam)
+
         given = sio.loadmat('hw3_1_soln_data.mat') #1 marker
         # given = sio.loadmat('hw3_4_soln_data.mat') #3 markers
         self.z_btr = given['bearing'] #only for 1 marker
@@ -64,6 +78,8 @@ class Rob2Wh:
         self.xtr = given['x']
         self.ytr = given['y']
 
+
+
     def vel_motion_model(self, vc, wc, x, y, th):
 
         v_hat = vc + np.random.normal(0, np.sqrt(self.a1*vc**2+self.a2*wc**2))
@@ -74,35 +90,6 @@ class Rob2Wh:
         th1 = wrap(th + w_hat*self.dt)
 
         return x1, y1, th1, v_hat, w_hat
-
-    def animation(self, x, y, th):
-        xm = -10
-        xM = 10
-        ym = -10
-        yM = 10
-
-        # First set up the figure, the axis, and the plot element we want to animate
-        fig = plt.figure()
-        ax = plt.axes(xlim=(0, 2), ylim=(-2, 2))
-        line, = ax.plot([], [], lw=2)
-
-        # call the animator.  blit=True means only re-draw the parts that have changed.
-        anim = animation.FuncAnimation(fig, self.animate, init_func=self.animation_init(line),
-                               frames=200, interval=20, blit=True)
-
-        plt.show()
-
-
-    # initialization function: plot the background of each frame
-    def animation_init(self, line):
-        line.set_data([], [])
-        return line,
-
-    def animate(self, i):
-        x = np.linspace(0, 2, 1000)
-        y = np.sin(2 * np.pi * (x - 0.01 * i))
-        line.set_data(x, y)
-        return line,
 
     def simulate_sensor(self, x, y, th, noise = 1):
 
@@ -150,80 +137,68 @@ class Rob2Wh:
         sap3 = np.concatenate((z2x3,z2x2,Q), axis=1)
         Sig_a_prev = np.concatenate((sap1, sap2, sap3), axis=0)
 
-        #generate sigma points
-        n = 7 # #u+#x+#z
-
-        # k = 1 # no specific values for k and alpha.  I will use the recommended values on the slides
-        # alpha = .5 #?
-        # Beta = 2
-        k = 4 # no specific values for k and alpha.  I will use the recommended values on the slides
-        alpha = .4 #?
-        Beta = 2
-
-        lam = alpha**2*(n+k)-n
-        gm = np.sqrt(n+lam)
         #generating sigma point.  we use the cholesky factor here.
         L = np.linalg.cholesky(Sig_a_prev)
-        Xa_prev = np.concatenate((mu_a_prev, mu_a_prev+gm*L,\
-                     mu_a_prev-gm*L), axis = 1)
+        Xa_prev = np.concatenate((mu_a_prev, mu_a_prev+self.gm*L,\
+                     mu_a_prev-self.gm*L), axis = 1)
 
         Xa_prev[2] = wrap(Xa_prev[2])
         Xa_prev[6] = wrap(Xa_prev[6])
 
         #pass sigma points through motion model and compute Guassian
-        #adding u to each column of Xs_u.  Each column gets passed in individually
+        #adding u to each column of Xk_u.  Each column gets passed in individually
         #there should be a for loop here
-        Xs_x_prev = Xa_prev[0:3]
-        Xs_u = Xa_prev[3:5]
-        Xs_z = Xa_prev[5:7]
+        Xk_x_prev = Xa_prev[0:3]
+        Xk_u = Xa_prev[3:5]
+        Xk_z = Xa_prev[5:7]
 
         #calculate weights
         wm = []
         wc = []
-        wm.append(lam/(n+lam))
-        wc.append(wm[0]+1-alpha**2+Beta)
+        wm.append(self.lam/(self.n+self.lam))
+        wc.append(wm[0]+1-self.alpha**2+self.Beta)
 
-        for i in range(1,2*n+1):
-            wm.append(1/(2*(n+lam)))
+        for i in range(1,2*self.n+1):
+            wm.append(1/(2*(self.n+self.lam)))
             wc.append(wm[i])
 
         wm = np.array([wm])
         wc = np.array([[wc]]).T
         if marker == 0:
-            Xs_x_bar = self.propagate(ut,Xs_u,Xs_x_prev)
+            Xk_x_bar = self.propagate(ut,Xk_u,Xk_x_prev)
 
             mu_bar = np.zeros((3,1))
-            mu_bar = (wm@Xs_x_bar.T).T
+            mu_bar = (wm@Xk_x_bar.T).T
             mu_bar[2] = wrap(mu_bar[2])
 
 
             Sig_bar = np.zeros((3,3))
-            for i in range(2*n+1):
-                term2 = np.array([Xs_x_bar[:,i]]).T-mu_bar
+            for i in range(2*self.n+1):
+                term2 = np.array([Xk_x_bar[:,i]]).T-mu_bar
                 term2[2] = wrap(term2[2])
                 Sig_bar = Sig_bar+wc[i]*term2@term2.T
             # set_trace()
         else:
-            Xs_x_bar = Xs_x_prev
+            Xk_x_bar = Xk_x_prev
             mu_bar = mu_prev
             Sig_bar = Sig_prev
             # mu2 = np.zeros((3,1))
-            # mu2 = (wm@Xs_x_bar.T).T
+            # mu2 = (wm@Xk_x_bar.T).T
             # Sig2 = np.zeros((3,3))
             # for i in range(2*n+1):
-            #     Sig2 = Sig2 +wc[i]*(np.array([Xs_x_bar[:,i]]).T-mu2)@(np.array([Xs_x_bar[:,i]]).T-mu2).T
+            #     Sig2 = Sig2 +wc[i]*(np.array([Xk_x_bar[:,i]]).T-mu2)@(np.array([Xk_x_bar[:,i]]).T-mu2).T
 
-        Zbar = self.sigma_measurements(Xs_x_bar,Xs_z, marker)
+        Zbar = self.sigma_measurements(Xk_x_bar,Xk_z, marker)
         zhat = (wm@Zbar).T
         zhat[1] = wrap(zhat[1])
         St = np.zeros((2,2))
         Sig_xz = np.zeros((3,2))
-        for i in range(2*n+1):
+        for i in range(2*self.n+1):
             term1 = np.array([Zbar[i]]).T-zhat
             term1[1] = wrap(term1[1])
             St = St+wc[i]*term1@term1.T
-        for i in range(2*n+1):
-            Sig_xz = Sig_xz + wc[i]*np.array([np.array([Xs_x_bar[:,i]]).T-mu_bar])@(np.array([Zbar[i]]).T-zhat).T
+        for i in range(2*self.n+1):
+            Sig_xz = Sig_xz + wc[i]*np.array([np.array([Xk_x_bar[:,i]]).T-mu_bar])@(np.array([Zbar[i]]).T-zhat).T
         Sig_xz = np.squeeze(Sig_xz)
 
         #measurement update for each landmark is done individually
@@ -239,7 +214,7 @@ class Rob2Wh:
         return(mu_t, Sig_t, Kt)
     #
 
-    def propagate(self, ut, Xs_u, state_prev):
+    def propagate(self, ut, Xk_u, state_prev):
         #pass in cholesky requirements and return cholesky_x
         # of the 15 vc and wc, 13 will be the same value
 
@@ -249,7 +224,7 @@ class Rob2Wh:
         x_prev = np.array([state_prev[0]]).T
         y_prev = np.array([state_prev[1]]).T
         th_prev = np.array([state_prev[2]]).T
-        length = len(Xs_u[0])
+        length = len(Xk_u[0])
         xt = []
         yt = []
         tht = []
@@ -258,8 +233,8 @@ class Rob2Wh:
         Xbar_x = []
 
         for i in range(length):
-            vhat = ut[0]+Xs_u[0][i]
-            what = ut[1]+Xs_u[1][i]
+            vhat = ut[0]+Xk_u[0][i]
+            what = ut[1]+Xk_u[1][i]
             x_new = x_prev[i] - vhat/what*math.sin(th_prev[i])+vhat/what*math.sin(wrap(th_prev[i]+what*self.dt))
             y_new = y_prev[i] + vhat/what*math.cos(th_prev[i])-vhat/what*math.cos(wrap(th_prev[i]+what*self.dt))
             th_new = wrap(th_prev[i] + what*self.dt)
@@ -277,17 +252,17 @@ class Rob2Wh:
         return(Xbar_x)
     #
 
-    def sigma_measurements(self, Xbar_x, xs_z, marker):
-        xs_z = np.squeeze(xs_z)
+    def sigma_measurements(self, Xbar_x, Xk_z, marker):
+        Xk_z = np.squeeze(Xk_z)
         Z_bar = np.zeros((15,2))
         noise = 0
         # z_meas = self.simulate_sensor(Xbar_x[0][0],Xbar_x[1][0],Xbar_x[2][0], noise)
         # Z_bar1 = np.array([z_meas[marker],z_meas[marker+3]])
-        # Z_bar[0,:] = np.array(Z_bar1+np.array([xs_z[:,0]]).T).T
+        # Z_bar[0,:] = np.array(Z_bar1+np.array([Xk_z[:,0]]).T).T
         for i in range(15):
             z_meas = self.simulate_sensor(Xbar_x[0][i],Xbar_x[1][i],Xbar_x[2][i], noise)
             Z_bar1 = np.array([z_meas[marker],z_meas[marker+3]])
-            Z_bar[i,:] = np.array(Z_bar1+np.array([xs_z[:,i]]).T).T
+            Z_bar[i,:] = np.array(Z_bar1+np.array([Xk_z[:,i]]).T).T
             Z_bar[i,1] = wrap(Z_bar[i,1])
         return Z_bar
 
@@ -295,6 +270,34 @@ class Rob2Wh:
     #     phi_new = (phi+np.pi)%(2*np.pi)-np.pi
     #     # phi_new = phi
     #     return phi_new
+
+    def animation(self, x, y, th):
+        xm = -10
+        xM = 10
+        ym = -10
+        yM = 10
+
+        # First set up the figure, the axis, and the plot element we want to animate
+        fig = plt.figure()
+        ax = plt.axes(xlim=(0, 2), ylim=(-2, 2))
+        line, = ax.plot([], [], lw=2)
+
+        # call the animator.  blit=True means only re-draw the parts that have changed.
+        anim = animation.FuncAnimation(fig, self.animate, init_func=self.animation_init(line),
+                               frames=200, interval=20, blit=True)
+
+        plt.show()
+
+    # initialization function: plot the background of each frame
+    def animation_init(self, line):
+        line.set_data([], [])
+        return line,
+
+    def animate(self, i):
+        x = np.linspace(0, 2, 1000)
+        y = np.sin(2 * np.pi * (x - 0.01 * i))
+        line.set_data(x, y)
+        return line,
 
     def plotting(self,x_hat, x, y_hat, y, th_hat, th, vc, v, wc, w, t, xe, ye, the, ve, we, K, sig_x, sig_y, sig_th):
 
@@ -320,35 +323,35 @@ class Rob2Wh:
         sigth_hi = 2*np.sqrt(sig_th)
         sigth_lo = -2*np.sqrt(sig_th)
 
-        fig1, axs = plt.subplots(3)
+        fig1, aXk = plt.subplots(3)
         fig1.suptitle("x, y, and theta vs. Time")
-        axs[0].plot(t, x_hat, label = "x [m]")
-        axs[0].plot(t, x, label = "true x [m]")
-        axs[0].legend(loc = "upper right")
-        axs[1].plot(t, y_hat, label="y [m]")
-        axs[1].plot(t, y, label="true y [m]")
-        axs[1].legend(loc = "upper right")
-        axs[2].plot(t, th_hat, label = "theta [rad]")
-        axs[2].plot(t, th, label = "true theta [rad]")
-        axs[2].legend(loc = "upper right")
-        axs[2].set_xlabel('time [s]')
+        aXk[0].plot(t, x_hat, label = "x [m]")
+        aXk[0].plot(t, x, label = "true x [m]")
+        aXk[0].legend(loc = "upper right")
+        aXk[1].plot(t, y_hat, label="y [m]")
+        aXk[1].plot(t, y, label="true y [m]")
+        aXk[1].legend(loc = "upper right")
+        aXk[2].plot(t, th_hat, label = "theta [rad]")
+        aXk[2].plot(t, th, label = "true theta [rad]")
+        aXk[2].legend(loc = "upper right")
+        aXk[2].set_xlabel('time [s]')
         fig1.show()
 
-        fig2, axs = plt.subplots(3)
+        fig2, aXk = plt.subplots(3)
         fig2.suptitle("Covariance & Error vs. Time")
-        axs[0].plot(t,xe, label="x error [m]")
-        axs[0].plot(t,sigx_hi, label="upper covariance")
-        axs[0].plot(t,sigx_lo, label="lower covariance")
-        axs[0].legend(loc = "upper right")
-        axs[1].plot(t,ye, label="y error [m]")
-        axs[1].plot(t,sigy_hi, label="upper covariance")
-        axs[1].plot(t,sigy_lo, label="lower covariance")
-        axs[1].legend(loc = "upper right")
-        axs[2].plot(t,the, label="theta error [rad]")
-        axs[2].plot(t,sigth_hi, label="upper covariance")
-        axs[2].plot(t,sigth_lo, label="lower covariance")
-        axs[2].legend(loc = "upper right")
-        axs[2].set_xlabel('time [s]')
+        aXk[0].plot(t,xe, label="x error [m]")
+        aXk[0].plot(t,sigx_hi, label="upper covariance")
+        aXk[0].plot(t,sigx_lo, label="lower covariance")
+        aXk[0].legend(loc = "upper right")
+        aXk[1].plot(t,ye, label="y error [m]")
+        aXk[1].plot(t,sigy_hi, label="upper covariance")
+        aXk[1].plot(t,sigy_lo, label="lower covariance")
+        aXk[1].legend(loc = "upper right")
+        aXk[2].plot(t,the, label="theta error [rad]")
+        aXk[2].plot(t,sigth_hi, label="upper covariance")
+        aXk[2].plot(t,sigth_lo, label="lower covariance")
+        aXk[2].legend(loc = "upper right")
+        aXk[2].set_xlabel('time [s]')
         fig2.show()
 
         fig3 = plt.figure(5)
