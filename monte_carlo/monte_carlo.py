1b3583c8f3620a93166f724451e735fe27bfccc4
Matt Rydalch <matthewk.rydalch@gmail.com>
2019-10-09 08:14:03 -0600

    added comments from class

--- monte_carlo/monte_carlo.py
+++ monte_carlo/monte_carlo.py
@@ -17,16 +17,21 @@ from rob_2wh import Rob2Wh
 
 class Monte_Carlo:
     def monte_carlo(self,Xk_prev, ut, zt, m, M):
-        
+
         rob = Rob2Wh()
 
         #Xkt are particles
-        Xk_bar = Xkt = phi
+        Xk_bar = Xkt = phi #phi is an empty vector
         for m in range(M):
-            xm = rob.vel_motion_model(ut,xm_prev)
-            wm = measurement_model(zt,xm,m)
-            Xk_bar = Xk_bar+inner_product(xm,wm)
+            xm = rob.vel_motion_model(ut,xm_prev) #be sure noise is included
+            wm = self.measurement_model(zt,xm,m) #weight, this actually is a probability calculation using meas model
+            Xk_bar = Xk_bar+inner_product(xm,wm) #adds particles to their weights
 
+        #prediction: draw from the proposal
+        #correction: weighting by the ratio of target and proposal
+
+        #resampling
+        #look at low variance sampler algorithm
         for m in range(M):
             drawi() #draw i with probabilty alpha*wt
             addxi() # add xi to Xkt
@@ -43,6 +48,10 @@ class Monte_Carlo:
     def measurement_model(self, Xbar_x, Xk_z, marker):
 
         return Z_bar
+
+    def low_var_sampler(self, Xkt, Wt):
+
+        return(Xk_bar)
 
         # def wrap(self, phi):
         #     phi_new = (phi+np.pi)%(2*np.pi)-np.pi
