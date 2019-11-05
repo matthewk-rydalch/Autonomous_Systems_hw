21fed24c9870deeac924db89d74f00b0f98ffc25
Matt Rydalch <matthewk.rydalch@gmail.com>
2019-11-05 09:03:39 -0700

    flipped plotting, but robot isn't on the map anymore?

--- occupancy_grid/animator.py
+++ occupancy_grid/animator.py
@@ -12,9 +12,19 @@ class Animator:
 		x_size = 100*scale
 		y_size = 100*scale
 		size = (x_size, y_size)
+		th = -np.pi/2
+		rotate = np.array([[np.cos(th), -np.sin(th)],\
+				          [np.sin(th), np.cos(th)]])
+		grid_rotate = np.zeros((len(grid),len(grid)))
+		for i in range(len(grid)):
+			for j in range(len(grid)):
+				rotate_ind = np.array([[i, j]])@rotate
+				grid_rotate[int(rotate_ind[0][0])][int(rotate_ind[0][1])] = grid[i][j]
 		
-		resized = cv2.resize(grid.T, size, interpolation = cv2.INTER_AREA)
-		location = (int(xt[0]*scale), int(xt[1]*scale))
+		resized = cv2.resize(grid_rotate, size, interpolation = cv2.INTER_AREA)
+		# location = (int(xt[0]*scale), int(xt[1]*scale))
+		location = (int(xt[0]*scale), int(xt[1]*scale))@rotate
+		location = (int(location[0]),int(location[1]))
 		cv2.circle(resized,location,2*scale,(255,255,0),-1)
 		cv2.imshow('grid', resized)
 		cv2.waitKey(1)
