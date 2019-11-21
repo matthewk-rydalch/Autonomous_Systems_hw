c3fd8f3988b3ddaa168fea492313a26ef5540289
Matt Rydalch <matthewk.rydalch@gmail.com>
2019-11-20 17:46:30 -0700

    I think I made it worse

--- fast_slam/slam_fast.py
+++ fast_slam/slam_fast.py
@@ -20,18 +20,6 @@ class Slam:
         self.Mtr = Mtr
         self.particles = particles
         self.p0 = 1.0/self.particles
-        #calculate Fxj's since they are constant
-        self.Fxj = []
-        for j in range(1,N+1):
-            mat1 = np.eye(3,3)
-            mat2 = np.zeros((2,3))
-            mat3 = np.zeros((5,2*j-2))
-            mat4 = np.zeros((3,2))
-            mat5 = np.eye(2,2)
-            mat6 = np.zeros((5,2*N-2*j))
-            mat7 = np.concatenate((mat1,mat2),axis=0)
-            mat8 = np.concatenate((mat4,mat5),axis=0)
-            self.Fxj.append(np.concatenate((mat7.T,mat3.T,mat8.T,mat6.T),axis=0).T)
     
         self.Qt = np.array([[self.sig_r**2, 0],\
              [0, self.sig_phi**2]])
@@ -41,9 +29,6 @@ class Slam:
         #initialize variables to hold particles before resampling
         Y_new = []
         w_new = []
-        # if len(ct) == 0:
-        #     a=1
-        # j = ct[time_step%len(ct)]# j is the landmarker being observed this time step
 
         ###for each particle
         for k in range(self.particles):
@@ -60,43 +45,35 @@ class Slam:
             Sig_k = Sig_p
             wj = np.ones(len(Muk))
             wk = 1
-            for j in ct:
-                if self.j[k][j]==0:  #initialize marker if it has not been seen already
+            j = ct[time_step%len(ct)]
+            # for j in ct:
+            if self.j[k][j]==0:  #initialize marker if it has not been seen already
 
-                    self.j[k][j]=1 #variable used to tell if already initialized
-                    Muk[j] = self.initialize_marker(Zt[:,j], Xt) #h**-1
-                    H = self.meas_jacobian(Xt, Muk[j]) #h prime
-                    Sig_k[j] = np.array(inv(H)@self.Qt@inv(H).T)
-                    wj[j] = np.array([[self.p0]]) #default importance weight
-                elif self.j[k][j]==1:
-                    zhat, c_ignore = self.h(Xt, Muk[j], fov = 2*np.pi, noise = 0) #for this leave the fov at 2 pi.  Markers are already canceled out by ct
-                    H = self.meas_jacobian(Xt, Muk[j]) #h prime
-                    Q = H@Sig_p[j]@H.T+self.Qt
-                    K = Sig_p[j]@H.T@inv(Q)
-                    Muk[j] = Mup[j] + K@(Zt[:,j]-np.squeeze(zhat))
-                    Muk[j][0][1] = utils.wrap(Muk[j][0][1])
-                    Sig_k[j] = (np.eye(2)-K@H)@Sig_p[j]
-                    wj[j] = np.linalg.det(2*np.pi*Q)**(-1/2)*np.exp(-1/2*(Zt[:,j]-zhat.T)@inv(Q)@(Zt[:,j]-zhat.T).T)
-                    wk = wk*wj[j]
+                self.j[k][j]=1 #variable used to tell if already initialized
+                Muk[j] = self.initialize_marker(Zt[:,j], Xt) #h**-1
+                H = self.meas_jacobian(Xt, Muk[j]) #h prime
+                Sig_k[j] = np.array(inv(H)@self.Qt@inv(H).T)
+                wj = np.array([[self.p0]]) #default importance weight
+            elif self.j[k][j]==1:
+                zhat, c_ignore = self.h(Xt, Mup[j], fov = 2*np.pi, noise = 0) #for this leave the fov at 2 pi.  Markers are already canceled out by ct
+                H = self.meas_jacobian(Xt, Mup[j]) #h prime
+                Q = H@Sig_p[j]@H.T+self.Qt
+                K = Sig_p[j]@H.T@inv(Q)
+                Muk[j] = Mup[j] + K@(Zt[:,j]-np.squeeze(zhat))
+                Muk[j][0][1] = utils.wrap(Muk[j][0][1])
+                Sig_k[j] = (np.eye(2)-K@H)@Sig_p[j]
+                wj = np.linalg.det(2*np.pi*Q)**(-1/2)*np.exp(-1/2*(Zt[:,j]-zhat.T)@inv(Q)@(Zt[:,j]-zhat.T).T)
+                if wj == 0:
+                    bdsas = 1
+                    # print('wj = ', wj[j])
             Y_new.append([Xt, Muk, Sig_k])
-            # wk = np.prod(wj)
-            w_new.append(wk)
-        wp_norm = self.normalize(w_new)
-
-        # # make weights vector and normalize
-        # w_new = np.array(w_new)
-        # weight_sum = sum(np.squeeze(w_new))
-        # print('weight sum = ', weight_sum)
-        # wp_norm = np.squeeze(w_new) / np.squeeze(weight_sum)
+            w_new.append(wj)
+        wp_norm, w_sum = self.normalize(w_new)
         max_weight_ind = np.argmax(wp_norm)
+
         Yt = utils.low_var_sampler(Y_new, wp_norm)
         if Yt[0][0][0] == Yt[99][0][0]:
             print('possible particle deprevation issue')
-            Ya = np.array(Yt).T
-            # print('X = ',Ya[0])
-            # print('Mu = ',Ya[1])
-            # print('Sig = ',Ya[2])
-            # set_trace()
 
         return(Yt, max_weight_ind)
     #
@@ -123,19 +100,6 @@ class Slam:
         muj = np.array([mujx, mujy]).T
 
         return muj
-
-    def uniform_point_cloud(self, xgrid, ygrid, k):
-
-        # #initialize point or particle cloud
-        Xk_prev = np.zeros((3,k))
-
-        #for each particle determine a random selection of states from a uniform distribution
-        for i in range(k):
-            Xk_prev[0][i] = np.random.uniform(low=xgrid[0], high=xgrid[1], size=None)
-            Xk_prev[1][i] = np.random.uniform(low=ygrid[0], high=ygrid[1], size=None)
-            Xk_prev[2][i] = np.random.uniform(low= -math.pi, high = math.pi, size = None)
-
-        return(Xk_prev)
     
     def retrieve(self, Yp, k):
         Ypk = Yp[k]
@@ -149,7 +113,8 @@ class Slam:
 
         weight_sum = sum(w)
         print('weight sum = ', weight_sum)
+
         w_norm = np.squeeze(w) / np.squeeze(weight_sum)
 
-        return w_norm
+        return w_norm, weight_sum
 
