8915bdc3b9eb5b6c221e3b4f40063f6e9245876b
Matt Rydalch <matthewk.rydalch@gmail.com>
2019-12-02 10:54:31 -0700

    added pomdp

---
+++ pomdp/driver_pomdp.py
@@ -0,0 +1,26 @@
+# Implement algorithm 15.1 with the objective of representing the value function for the illustrative example of section 15.2
+# Compute the value function for the example for a time horizon of 2 and show that you obtain the linear value function constraints of equation 15.31 (along with a few others that have not been pruned away).
+# Develop a simple pruning strategy to prune away some of the obviously superfluous constraints (replicates of initial payoff constraints, constraints dominated by (lying below) the payoff constraint for u3).
+# Modify the state transition probability for u3 as well as the measurement probabilities for states x1 and x2. Compute value functions for different probabilities. Do your results make sense? Change the payoffs associated with the control actions and compute the value function. Do the results make sense?
+# Using the probability and payoff parameters of your choice, use the associated value function to choose control actions. Assume that your true initial state is x1 and that your belief is 0.6. What outcomes do you obtain for 10 trials? Do you outcomes align with your expectations? Did your value function produce good results?
+
+import numpy as np
+import scipy
+from IPython.core.debugger import set_trace
+import scipy.io as sio
+import copy
+from importlib import reload, import_module
+visuals = reload(import_module("visuals"))
+from visuals import Viz
+pomdp = reload(import_module("pomdp"))
+from pomdp import POM
+
+###parameters###
+T = 2 #planning horizon, 2 comes from problem statement 2
+
+#instatiate objects
+pom = POM()
+viz = Viz()
+
+#run algorithm
+G = pom.value_function(T) #G is gamma, which is a set of parameter vectors that specify a linear function over the belief space
---
+++ pomdp/pomdp.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+class POM:
+    def __init__():
+
+    def value_function(T):
+        G = np.zeros((1,n)) #how big? 
+        for tau in range(1,T): #check this range when you understand the horizon.  Should it be 1 or 0
+            Gp = zeros(()) #how big?
+            for u in ut: #all control actions u
+                for z in zt: #all measurments z
+                    for j in range(1,N):
+                        v[u][z][j] = np.sum(v[i]*p(z|xi)*p(xi|u,xj) #what are the k superscripts on the v's.  What are the other elements values of v[i] (3d)
+
+            for u in ut:
+                for k = np.ones(len(k)):np.ones(len(k))*abs(G): #need to modify the syntax
+                    for i in range(1,N):
+                        vp[i] = g*(r(xi,u)+sum(v[u][z][i])) #what is the function r?  v has a superscript k(z)?
+                    Gp[] = [u,vp] #adding (u:v'1, v'2, ...) to Gp
+
+
